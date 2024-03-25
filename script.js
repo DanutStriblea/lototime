8e7ed30106ec82912f99d9ce3d7e9d1b36dc773d
Danut Striblea <118006000+DanutStriblea@users.noreply.github.com>
2024-03-25 22:28:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function generareNumereLoto() {
     const interval = [1, 49];
-    let numereLoto = [];
+    let numereLoto = new Set(); // Folosim un Set pentru a asigura unicitatea numerelor generate
 
     // Generăm un seed bazat pe data și ora curentă
     const seed = new Date().getTime();
@@ -9,11 +9,11 @@ function generareNumereLoto() {
     Math.seed = seed;
 
     // Generăm 6 numere aleatoare folosind seed-ul
-    for (let i = 0; i < 6; i++) {
+    while (numereLoto.size < 6) { // Continuăm să generăm numere până când avem 6 numere unice
         const numar = Math.floor(Math.random() * (interval[1] - interval[0] + 1)) + interval[0];
-        numereLoto.push(numar);
+        numereLoto.add(numar); // Adăugăm numărul în Set
     }
 
     const rezultat = document.getElementById("rezultat");
-    rezultat.innerHTML = "<div class='numere'>" + numereLoto.map(numar => `<span>${numar}</span>`).join("") + "</div>";
+    rezultat.innerHTML = "<div class='numere'>" + [...numereLoto].map(numar => `<span>${numar}</span>`).join("") + "</div>"; // Folosim spread operator pentru a converti Set-ul într-un array și apoi afișăm numerele
 }
